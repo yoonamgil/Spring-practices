544d1645618e2e2f8663a37568770896e17c1d1d
yoonamgil <skaska5@naver.com>
2020-02-20 14:30:03 +0900

    추가

--- hellospring/src/main/java/com/douzone/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/douzone/hellospring/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.douzone.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -11,4 +13,29 @@ public class HelloController {
 		
 		return "/WEB-INF/views/hello.jsp";
 	}
+	
+	@RequestMapping("/hello2")
+	public String hello2(String name) {
+		
+		System.out.println(name);
+		return "/WEB-INF/views/hello2.jsp";
+	}
+	
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("name",name);
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		
+		
+		
+		return mav;
+	}
+	
+	@RequestMapping("/hello4")
+	public String hello4(String name, Model model) {
+		model.addAttribute("name",name); // 이 방법을 추천 
+		return "/WEB-INF/views/hello4.jsp";
+	}
+	
 }
